8f0f09fd7f1d879b9944299d3b2a62c3491cca4a
SilvaSilvio <silviosilva.ads@gmail.com>
2022-06-17 10:58:28 -0300

    Criado o metodo get findAll para categoria utilizando padrão DTO

--- src/main/java/com/silva/xforce/resource/CategoriaResource.java
+++ src/main/java/com/silva/xforce/resource/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.silva.xforce.resource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.silva.xforce.domain.Categoria;
+import com.silva.xforce.dtos.CategoriaDTO;
 import com.silva.xforce.service.CategoriaService;
 
 @RestController
@@ -22,6 +26,15 @@ public class CategoriaResource {
 		Categoria obj = categoriaService.findById(id);
 		return ResponseEntity.ok().body(obj);
 
+	}
+
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = categoriaService.findAll();
+		//Converte uma Categoria em Categoria DTO
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
--- src/main/java/com/silva/xforce/service/CategoriaService.java
+++ src/main/java/com/silva/xforce/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.silva.xforce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,17 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-
+	
+	//Faz a busca por id na entidade categoria
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto nao encontrado! " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	//Busca todas os registros na entidade categoria...
+	public List<Categoria> findAll() {
+		return categoriaRepository.findAll();
 	}
 
 }
