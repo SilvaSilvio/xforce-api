70f2821d1b13aeb052442c70ee483acd0159b657
SilvaSilvio <silviosilva.ads@gmail.com>
2022-06-17 11:44:48 -0300

    Criado o metodo update para a classe categoria, tanto no service, bem como no controller

--- src/main/java/com/silva/xforce/resource/CategoriaResource.java
+++ src/main/java/com/silva/xforce/resource/CategoriaResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +50,14 @@ public class CategoriaResource {
 
 		return ResponseEntity.created(uri).body(obj);
 	}
+	
+	//Atualização de Cagetoria
+	@PutMapping(value= "/{id}")
+	public ResponseEntity<CategoriaDTO> updateCategoria(@PathVariable Integer id, @RequestBody CategoriaDTO objDTO){
+		Categoria categ = categoriaService.updateCategoria(id, objDTO);
+		return ResponseEntity.ok().body(new CategoriaDTO(categ));
+	}
+	
+	
 
 }
